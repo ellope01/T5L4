230e3245e253e40df3f59c0dcc8c7a2f7244436e
Alfonso <algagu01@floridauniversitaria.es>
2023-01-27 17:09:17 +0100

    del2al9real

--- del2al9.sh
+++ del2al9.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-cat disc.txt | df -h | awk '{if ($5 >= 2 && $5 <= 9) print $6}' >> sol.txt
+limite=`cat disc.txt | wc -l`
+
+for i in `seq 2 $limite`
+do
+	porcen=`cat disc.txt | head -n$i| tail -1 | awk '{print $5}' | sed 's/%//g'`
+		if [ $porcen -ge 2 ] && [ $porcen -le 9 ]
+		then
+			`cat disc.txt | head -n$i | tail -1 >> sol.txt`
+		fi
+done
