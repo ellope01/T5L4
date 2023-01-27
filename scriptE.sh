306de69891a956476fefc29dc7b987469373c8ab
Elizabeth Lopez <ellope01@floridauniversitaria.es>
2023-01-27 17:00:47 +0100

    solElizabeth

---
+++ scriptE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+lineas=`cat disc.txt | wc -l`
+
+for i in `seq 2 $lineas`
+do 
+    porcentaje=`cat disc.txt | head -n$i | tail -1 | awk '{print $5}' | sed 's/%//g'`
+
+    if [ $porcentaje -ge 41 ] && [ $porcentaje -le 50 ]
+    then
+        `cat disc.txt | head -n$i | tail -1 >> sol.txt`
+    fi
+    
+done
